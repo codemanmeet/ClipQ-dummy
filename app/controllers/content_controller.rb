dfd6ec1976ffbcdcea8fd9c08924a21453a89541
Christie Hua <christiehua@gmail.com>
2014-09-06 21:24:41 -0700

    content controller

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,4 +1,69 @@
 class ContentController < ApplicationController
+
   def index
+  	@content = Content.find_by_sql('SELECT * FROM contents ORDER BY created_at DESC')
+  end
+
+  def compindex
+  	@content = Content.find_by_sql('SELECT * FROM contents WHERE approved=false')
+  end
+
+  def show
+  	@content = Content.find(params[:id])
+  end
+
+  def new 
+  	@content = Content.new
+  end
+
+  def create
+  	@content = Content.new(content_params)
+  	if @content.save(content_params)
+  		redirect_to contents_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  	@content = Content.find(params[:id])
+  end
+
+  def update
+  	@content = Content.find(params[:id])
+  	if @content.update(content_params)
+  		redirect_to contents_compindex_path
+  	else 
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+    @content = Content.find(params[:id])
+    @content.destroy
+    redirect_to contents_path
+  end
+
+  def upvote
+    @content = Content.find(params[:id])
+    @contentvote = @content.contentvotes.build
+    @contentvote.upvote = 1
+    @contentvote.save
+    redirect_to contents_path
+  end
+
+  def downvote
+    @contents = Content.find(params[:id])
+    @contentvote = @content.contentvotes.build
+    @contentvote.downvote = 1
+    @contentvote.save
+    redirect_to contents_path
+  end
+
+  def contentvote_params
+    params.require(:contentvote).permit(:secret_id, :upvote, :downvote)
+  end
+  def content_params
+    params.require(:content).permit(:question, :video, :picture, :tag, :open)
   end
 end
